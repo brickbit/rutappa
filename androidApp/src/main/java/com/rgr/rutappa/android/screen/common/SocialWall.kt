d73fc166d24b0ee1e9098455bfceb278a37bcba4
Roberto García Romero <roberto.garcia@fcm.digital>
2025-02-07 19:12:23 +0100

    feat: Update footer

--- androidApp/src/main/java/com/rgr/rutappa/android/screen/common/SocialWall.kt
+++ androidApp/src/main/java/com/rgr/rutappa/android/screen/common/SocialWall.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
